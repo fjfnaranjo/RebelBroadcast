a9ec26f44a38bd3ef4d952d44c0a755d43533ffd
Ricardo Gómez Escalante <elgatil@gmail.com>
2018-01-27 00:42:42 +0100

    Badguys routing completed

--- scenes/RoutePoint.gd
+++ scenes/RoutePoint.gd
@@ -1,4 +1,10 @@
 extends Area2D
 
 func _ready():
-	pass
+	set_process(true)
+
+func _process(delta):
+	var bodies = get_overlapping_bodies()
+	for body in bodies:
+		if (body.is_in_group("badguys") && body.has_method("point_reached")):
+			body.point_reached(get_name())
