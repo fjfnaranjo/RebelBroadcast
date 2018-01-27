5d223dcb0bc8b70f83287dad46367266b68bb5dc
Juan R Diego <juanra.diego@gmail.com>
2018-01-27 19:53:47 +0100

    Newspaper sound sound early

--- scenes/Menu.gd
+++ scenes/Menu.gd
@@ -1,17 +1,11 @@
 extends Node
 
-var newspaper_sound = false
-
 func _ready():
 	set_process_input(true)
+	State.play_sample("newspaper_deployed")
 	if State.active_scene == null:
 		State.active_scene = self
 
 func _input(event):
 	if(event.is_action_pressed("ui_accept")):
 		State.change_scene("res://scenes/InGame.tscn")
-
-func _process(delta):
-	if(not newspaper_sound):
-		State.play_sample("newspaper_deployed")
-		newspaper_sound = true
