ac7edf5aa3ceca1b5feb4383fc1cfab4dffe6332
Ricardo Gómez Escalante <elgatil@gmail.com>
2018-01-27 13:10:34 +0100

    bugfix: bad variable name

--- scenes/Badguy.gd
+++ scenes/Badguy.gd
@@ -14,7 +14,7 @@ func _ready():
 	set_process(true)
 
 func _process(delta):
-	if(not State.active_scene.has_node(_route[_current_destination_idx])):
+	if(not State.active_scene.has_node(_route[_next_destination_idx])):
 		return
 	var target
 	if spotted_radio and spotted_radio.get_ref():
