89ee6b67cf81f43282146484b78f13fc4a391ade
Ricardo Gómez Escalante <elgatil@gmail.com>
2018-01-27 12:29:52 +0100

    Badguys now seek radios to destroy them

--- scenes/Badguy.gd
+++ scenes/Badguy.gd
@@ -1,34 +1,40 @@
 extends KinematicBody2D
 
 export var _route = [""]
-var _current_destination_idx
+var _next_destination_idx
 var _vision
+var spotted_radio = null
 
 const MAX_SPEED = 100
 
 func _ready():
 	add_to_group("badguys")
-	_current_destination_idx = 0
+	_next_destination_idx = 0
 	_vision = get_node("Vision")
 	set_process(true)
 
 func _process(delta):
-	var destination_RP = State.active_scene.get_node(_route[_current_destination_idx])
-	var remain = move((destination_RP.get_pos()-get_pos()).normalized()*delta*MAX_SPEED)
+	var target
+	if spotted_radio and spotted_radio.get_ref():
+		target = spotted_radio.get_ref()
+	else:
+		target = State.active_scene.get_node(_route[_next_destination_idx])
+	move((target.get_pos()-get_pos()).normalized()*delta*MAX_SPEED)
 	
-	var radio_spotted = false
 	var rebel_spotted = false
 	for area in _vision.get_overlapping_areas():
 		if area.is_in_group("radios"):
-			radio_spotted = true
+			spotted_radio = weakref(area)
 			break
 	for body in _vision.get_overlapping_bodies():
 		if body.is_in_group("rebel_guys"):
 			rebel_spotted = true
 			break
-	if (radio_spotted and rebel_spotted):
-		State.game_over()
+	if (spotted_radio and spotted_radio.get_ref()):
+		if rebel_spotted:
+			State.game_over()	
+	
 
 func point_reached(RP_name):
-	if(RP_name == _route[_current_destination_idx]):
-		_current_destination_idx = (_current_destination_idx + 1) % _route.size()
+	if(RP_name == _route[_next_destination_idx]):
+		_next_destination_idx = (_next_destination_idx + 1) % _route.size()
