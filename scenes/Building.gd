9228a4610aa4330c562992faaecb944a2a122b9d
Juan R Diego <juanra.diego@gmail.com>
2018-01-27 10:33:55 +0100

    Waves affects loyalty

--- scenes/Building.gd
+++ scenes/Building.gd
@@ -4,7 +4,16 @@ export var loyalty = 5
 
 func _ready():
 	set_process(true)
+	add_to_group("buildings")
 	get_node("Loyalty").set_text(str(loyalty))
 
 func _process(delta):
-	 get_node("Loyalty").set_text(str(loyalty))
+	if(loyalty == 0):
+		get_node("Loyalty").set_text(":)")
+	else:
+		get_node("Loyalty").set_text(str(loyalty))
+
+func hurt():
+	if (loyalty > 0):
+		loyalty -= 1
+
--- scenes/Wave.gd
+++ scenes/Wave.gd
@@ -1,9 +1,25 @@
 extends Area2D
 
+const SECONDS = 1
+
 export var radius = 100
+var countdown
+
 
 func _ready():
 	set_process(true)
+	countdown = SECONDS
 	var sprite_radius = get_node("Sprite").get_texture().get_width() / 2.0
 	get_parent().set_scale(Vector2(radius,radius)/sprite_radius)
 	
+func _process(delta):
+	countdown -= delta
+	if (countdown <= 0):
+		countdown = SECONDS
+		_check_collisions()
+	
+func _check_collisions():
+	var things = get_overlapping_bodies()
+	for thing in things:
+		if(thing.is_in_group("buildings") and thing.has_method("hurt")):
+			thing.hurt()
