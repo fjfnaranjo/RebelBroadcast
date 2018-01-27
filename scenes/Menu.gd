2556a7701a8a3b2f069386f82cbdcc6e4c523242
Juan R Diego <juanra.diego@gmail.com>
2018-01-27 19:50:45 +0100

    Bugfix in sample library. Also newspaper sample added

--- scenes/Menu.gd
+++ scenes/Menu.gd
@@ -1,5 +1,7 @@
 extends Node
 
+var newspaper_sound = false
+
 func _ready():
 	set_process_input(true)
 	if State.active_scene == null:
@@ -8,3 +10,8 @@ func _ready():
 func _input(event):
 	if(event.is_action_pressed("ui_accept")):
 		State.change_scene("res://scenes/InGame.tscn")
+
+func _process(delta):
+	if(not newspaper_sound):
+		State.play_sample("newspaper_deployed")
+		newspaper_sound = true
