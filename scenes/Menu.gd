14a2656153e9cb3fcccca370839d8d59b418adef
Juan R Diego <juanra.diego@gmail.com>
2018-01-27 20:02:16 +0100

    Newspaper sounds before main theme

--- scenes/Menu.gd
+++ scenes/Menu.gd
@@ -1,6 +1,11 @@
 extends Node
 
+var MUSIC_OFFSET = 3.0
+var time = 0.0
+var music_on = false
+
 func _ready():
+	set_process(true)
 	set_process_input(true)
 	State.play_sample("newspaper_deployed")
 	if State.active_scene == null:
@@ -9,3 +14,9 @@ func _ready():
 func _input(event):
 	if(event.is_action_pressed("ui_accept")):
 		State.change_scene("res://scenes/InGame.tscn")
+
+func _process(delta):
+	time += delta
+	if(time >= MUSIC_OFFSET and not music_on):
+		State.play_song("main_theme")
+		music_on = true
