ef97b4ff2f971897e8ad734207b9af64cb30755e
Ricardo Gómez Escalante <elgatil@gmail.com>
2018-01-28 01:06:59 +0100

    Radio delivery stations have cooldown

--- scenes/RadioPickUpArea.gd
+++ scenes/RadioPickUpArea.gd
@@ -1,12 +1,23 @@
 extends Area2D
 
+const MAX_COOLDOWN = 1.0
+var cooldown
+var radio_available = true
 
 func _ready():
 	add_to_group("radio_hideouts")
+	cooldown = MAX_COOLDOWN
 	set_process(true)
 
 func _process(delta):
-	var bodies = get_overlapping_bodies()
-	for body in bodies:
-		if (body.is_in_group("rebel_guys") && body.has_method("obtain_radio")):
-			body.obtain_radio()
+	if not radio_available:
+		cooldown -= delta
+		if cooldown <= 0:
+			radio_available = true
+			cooldown = MAX_COOLDOWN
+	else:
+		var bodies = get_overlapping_bodies()
+		for body in bodies:
+			if (body.is_in_group("rebel_guys") && body.has_method("obtain_radio")):
+				body.obtain_radio()
+				radio_available = false
