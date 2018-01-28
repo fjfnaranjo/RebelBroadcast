ad163cfb3bf973303f1306cff236180d15dc5b16
Ricardo Gómez Escalante <elgatil@gmail.com>
2018-01-28 02:50:56 +0100

    Improvements to Menu and Aftergame

--- scenes/Aftergame.gd
+++ scenes/Aftergame.gd
@@ -1,6 +1,6 @@
 extends Node
 
-const VICTORY_SENTENCES = [""]
+const VICTORY_SENTENCES = ["There are people  in the streets asking for the resignation of the Dictator. The Revolution begins now"]
 const DEFEAT_SENTENCES = [""]
 
 func _ready():
@@ -10,12 +10,12 @@ func _ready():
 	var result = ""
 	if (State.win):
 		State.play_song("victory_theme")
-		result = "VICTORY!!"
-		text = "Congratulations! You have liberated the Mother Land!"
+		result = "VICTORY!"
+		text = "There are people  in the streets asking for the resignation of the Dictator. The Revolution begins now"
 	else:
 		State.play_song("spotted_plus_gameover")
-		result = "FAILURE!!"
-		text = "You have been capture, tortured and murdered! The revolution has died!"
+		result = "DEFEAT!"
+		text = "You are in the hands of the Political Police. The Revolution is over before it started.."
 	
 	get_node("Result").set_text(result)
 	get_node("Text").set_text(text)
--- scenes/Menu.gd
+++ scenes/Menu.gd
@@ -2,7 +2,6 @@ extends Node
 
 const MUSIC_OFFSET = 8.0
 const LORE_TIME = 4.0
-const TEXT = "Press Intro to join the resistance"
 
 var time = 0.0
 var music_on = false
@@ -10,8 +9,9 @@ var music_on = false
 func _ready():
 	set_process(true)
 	set_process_input(true)
+	get_node("Controls").hide()
+	get_node("Helper").hide()
 	State.play_sample("newspaper_deployed")
-	get_node("Node2D/Text").set_text(TEXT)
 	if State.active_scene == null:
 		State.active_scene = self
 
@@ -20,6 +20,10 @@ func _input(event):
 		if(music_on == false):
 			State.play_song("main_theme")
 		State.change_scene("res://scenes/InGame.tscn")
+	if(event.is_action_pressed("ui_help")):
+		get_node("Helper").show()
+	elif(event.is_action_released("ui_help")):
+		get_node("Helper").hide()
 
 func _process(delta):
 	time += delta
@@ -28,14 +32,14 @@ func _process(delta):
 		music_on = true
 	
 	if(_must_show_panel(time)):
-		get_node("Node2D").show()
+		get_node("Controls").show()
 	else:
-		get_node("Node2D").hide()
+		get_node("Controls").hide()
 	
 	
 func _must_show_panel(time):
 	if(time <= LORE_TIME):
 		return false
-	if ( int(time)%2 == 0 or time >= MUSIC_OFFSET+3.0):
+	if ( int(time*2)%2 == 0 or time >= MUSIC_OFFSET+3.0):
 		return true
 	return false
